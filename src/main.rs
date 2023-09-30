89aa5b88003b6ae84a3f8e65f806d35b2128a6ae
phil <phil@bitbrain.co.za>
2023-09-30 12:55:04 +0200

    Updates to use the scoreboard lib. We're gonna want that for future challenges

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::env;
 mod config;
-mod db;
+use scoreboard_db::{Db, Score};
 mod debug_config;
-mod score;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = std::env::args().collect::<Vec<String>>();
@@ -11,9 +9,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::RunMode::from_args(&args)?;
     println!("Config: {:?}", config);
 
-    let db_pass = env::var("DB_PASSWORD").expect("DB_PASS must be set");
+    let db_pass = match option_env!("DB_PASSWORD") {
+        Some(pass) => pass,
+        None => {
+            return Err(
+                "This program needs to be compiled with the $DB_PASS env variable set".into(),
+            )
+        }
+    };
 
-    let mut db = db::Db::new(&db_pass, "23_3_1")?;
+    let mut db = Db::new("localhost", 3306, "code_challenge", db_pass, "23_3_1")?;
 
     db.insert_score("Charlie", "ls -l", 300)
         .expect("error inserting score");
@@ -23,7 +28,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("error inserting score");
     db.insert_score("Alice", "ls -l", 97)
         .expect("error inserting score");
-    db.get_scores().expect("error getting scores");
+    let scores: Vec<Score> = db.get_scores().expect("error getting scores");
+    println!("Scores: {:?}", scores);
     db.clear_table().expect("error clearing table");
 
     Ok(())
